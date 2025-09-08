b9382a09e1dc4cd933e84a270f3bb2291f37e760
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2025-09-08 14:04:35 -0300

    Iniciando Tela Lista e outro Teste

--- android-nativo/app/src/main/java/com/example/appparidade/MainActivity.java
+++ android-nativo/app/src/main/java/com/example/appparidade/MainActivity.java
@@ -7,9 +7,6 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button ButtonTelaAPI = findViewById(R.id.button_tela_API);
+        Button ButtonTelaLista = findViewById(R.id.button_tela_Lista);
 
         ButtonTelaAPI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TelaApiActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        ButtonTelaLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TelaListaActivity.class);
+                startActivity(intent);
+
             }
         });
 
