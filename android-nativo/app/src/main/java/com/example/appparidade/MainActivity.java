79ea868a9e28ea49c9b776663fcd42e0056026fb
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2025-09-12 16:30:43 -0300

    Adicionando Tema

--- android-nativo/app/src/main/java/com/example/appparidade/MainActivity.java
+++ android-nativo/app/src/main/java/com/example/appparidade/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button ButtonTelaAPI = findViewById(R.id.button_tela_API);
         Button ButtonTelaLista = findViewById(R.id.button_tela_Lista);
         Button ButtonNativo = findViewById(R.id.button_tela_Nativo);
+        Switch SwitchTema = findViewById(R.id.switch_Tema);
 
         ButtonTelaAPI.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        SwitchTema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentMode = AppCompatDelegate.getDefaultNightMode();
+                if(currentMode == AppCompatDelegate.MODE_NIGHT_YES){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+            }
+        });
+
 
     }
 }
